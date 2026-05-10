442f34fa9ec2b535c16d12cbe413ba8cfd2d5951
Sheng-Yu-Cheng <sean.sheng.yu.cheng@gmail.com>
2026-05-10 20:39:56 +0800

    debug

--- RTL/SpMDV.v
+++ RTL/SpMDV.v
@@ -127,7 +127,6 @@ module SpMDV
 			S_READ_POSITION: if (split == 2'd2 && count == 12'd4095) next_state = S_READ_BIAS;
 			S_READ_BIAS: if (row == 12'd255) next_state = S_START_READ_ELEMENT;
 			S_START_READ_ELEMENT: next_state = S_READ_ELEMENT;
-			S_READ_ELEMENT: 
 		endcase
 	end
 	// output logic
