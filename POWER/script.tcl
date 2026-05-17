ea5c5c4d2005fb6be24f77c720510faa258ac92e
Sheng-Yu-Cheng <sean.sheng.yu.cheng@gmail.com>
2026-05-18 02:55:53 +0800

    update power

--- POWER/script.tcl
+++ POWER/script.tcl
@@ -32,7 +32,7 @@ read_sdf -load_delay net ../SYN/Netlist/SpMDV_syn.sdf
 
 ## ===== Analysis Window =====
 read_vcd  -strip_path testbed/u_SpMDV  ../GATE/SpMDV.fsdb \
-          -time {248370 704070}
+          -time {183794 377134}
 update_power
 report_power
 report_power > SpMDV.power
